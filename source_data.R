34ab5e493c33c4e33594479f18f61afaed753c8f
arb3risht <55642056+arb3risht@users.noreply.github.com>
2021-05-12 20:54:09 -0700

    Added and processed granular data

--- source_data.R
+++ source_data.R
@@ -6,8 +6,61 @@
 # File locations
 partyVotesFile <- here('data/alb_parl21_elxn_party_votes.csv')
 voteSeatShareFile <- here('data/vote-seat-share.csv')
+rawPartyData <- here('data/party_votes_au.csv') # raw party data
+rawElxnData <- here('data/elxn_data_all.csv') # results at A.-U. level
+joinedElxnData <- here('data/elxn_data_joined.csv') # output of joined data
 
-# Read CSV data into tibbles
+# Read CSV data into tibbles for votes aggregated at the admin unit
 partyVotesRaw <- read_csv(partyVotesFile)
 voteSeatShare <- read_csv(voteSeatShareFile)
 
+# Read CSV data into tibbles for votes at the polling-station level
+rawPartyVotes <- read_csv(rawPartyData)
+rawQVVotes <- read_csv(rawElxnData)
+view(rawPartyVotes)
+view(rawQVVotes)
+
+# Pre-processing: Populate party votes from rawPartyVotes into the last 13
+# fields of rawQVVotes by joining on the PollingStation field of both sets
+noPartyResults <- nrow(rawPartyVotes)
+
+# for each record in rawQVVotes, populate party votes from rawPartyVotes
+# records to populate are numbers 11 to 23
+i <- 1
+for (j in 1:noPartyResults) {
+  # match by polling station to copy corresponding votes
+  i <- as.numeric(which(rawQVVotes$PollingStation == 
+               rawPartyVotes$PollingStation[j]))
+  
+  if (rawPartyVotes[j, "Party"] == "PSD") {
+    rawQVVotes[i, "PSD"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "PBK") {
+    rawQVVotes[i, "PBK"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "PLDSH") {
+    rawQVVotes[i, "PLDSH"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "BD") {
+    rawQVVotes[i, "BD"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "ABEOK") {
+    rawQVVotes[i, "ABEOK"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "LSI") {
+    rawQVVotes[i, "LSI"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "NTH") {
+    rawQVVotes[i, "NTH"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "LRE") {
+    rawQVVotes[i, "LRE"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "PDAN") {
+    rawQVVotes[i, "PDAN"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "ADR") {
+    rawQVVotes[i, "ADR"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "LN") {
+    rawQVVotes[i, "LN"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "PS") {
+    rawQVVotes[i, "PS"] <- rawPartyVotes[j, "Votes"]
+  } else if (rawPartyVotes[j, "Party"] == "Other") {
+    rawQVVotes[i, "Other"] <- rawPartyVotes[j, "Votes"]
+  }
+}
+
+# export the joined data for later reference
+# write the data set out
+write_csv(rawQVVotes, joinedElxnData, na = "NA", append = FALSE)
